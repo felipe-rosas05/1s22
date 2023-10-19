54b88b7603f9dadfd9433391cb1479b89cdd6118
Luis Felipe Rosas Monge <144190893+felipe-rosas05@users.noreply.github.com>
2023-10-18 19:20:08 -0600

    Add files via upload

---
+++ p2/p2p7_1s22.java
@@ -0,0 +1,10 @@
+public class p2p7_1s22{
+	public static void main(String[] args) {
+		System.out.println("numeros de parametros ingresados desde linea de comando"+ args.length);
+		for(int a=0;a<args.length;a++){
+			System.out.println("Valor #"+a+ ": "+args[a]);
+			
+		}
+
+	}
+}
